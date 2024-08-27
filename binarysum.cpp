30822f8341b588be91353b55061cc0e47f3a1bf0
solx7c0de <vishvam1596@gmail.com>
2024-08-27 16:27:38 +0530

    python oops and some more dsa in cpp

--- binarysum.cpp
+++ binarysum.cpp
@@ -1,6 +1,8 @@
 #include<iostream>
 using namespace std;
-float reverse (int a)
+
+
+int reverse (int a)
 {  int rev=0;
 
    while(a>0)
@@ -11,10 +13,10 @@ float reverse (int a)
     return rev;
 }
 
-int binsum(int x, int y, int z)
-{   int lastx, lasty, lastf, sum =0, carry =0, finnum =0, rev =0, p;
+int binsum(int x, int y)
+{   int lastx, lasty, lastf, sum =0, carry =0, finnum =0, p;
     
-    while(z>0)
+    while(x>0 || y>0)
     {
         lastx = x%10;
         x/= 10;
@@ -25,24 +27,26 @@ int binsum(int x, int y, int z)
         if(sum == 0)
         {
           lastf = 0;
+          carry = 0;
         }
         else if(sum == 1)
         {
           lastf = 1;
+          carry = 0;
         }
         else if(sum == 2)
         {
           lastf = 0;
           carry = 1;
         }
-        else
+        else 
+        
         {
           lastf = 1;
           carry = 1;
         }
-        
+      
         finnum = (finnum*10) + lastf;
-        
     }
      
     if(carry == 1)
@@ -57,14 +61,14 @@ int binsum(int x, int y, int z)
 
 
 int main()
-{    int a, b, l;
+{    int a, b;
     cout<<"enter two binary nums"<<endl;
     cin>>a>>b;
-    cout<<"enter length of the numbers"<<endl;
-    cin>>l;
+  
     
 
-   cout<<"the sum of : "<<a<<" and "<<b<< " is "<<binsum(a, b, l);
+   cout<<"the sum of : "<<a<<" and "<<b<< " is "<<binsum(a, b);
 
    return 0;
+   
 }
